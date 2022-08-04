335072ac890b1ec4e42488e898e0a4e005fe0d09
chegarty74 <chegarty74@yahoo.com>
2022-08-04 17:44:26 -0500

    Question about color function

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import { PieChart } from 'react-minimal-pie-chart';
 import { CategoryContext } from "../context/CategoryContext";
 import { CardContext } from "../context/CardContext";
@@ -7,22 +7,39 @@ function Wheel() {
   
   const { selected } = useContext(CategoryContext);
   const { answer, correct} = useContext(CardContext)
-  console.log(answer);
-  console.log(selected);
+  // console.log(answer);
+  // console.log(selected);
+  const [color, setColor] = useState("");
+
+  const colorHex = useCallback(()=>{
+      function assignColor(color) {
+        const colorCode = {
+          "geography": "#05b4e1",
+          "film_and_tv": "#b91274",
+          "history": "#fbd60a",
+          "arts_and_literature": "#8c4e09",
+          "science":"#6cac06",
+          "sport_and_leisure": "#b91274"
+        };
+        return color[colorCode] ?? "#efefef"
+      }
+  }, [selected, correct, answer])
+
+  setColor(colorHex)
+
+  console.log(color);
 
   const data = 
     [
-      { title: 'One', value: 60, key: 1, color:"#05b4e1" },
-      { title: 'Two', value: 60, key: 2, color: "#6cac06" },
-      { title: 'Three', value: 60, key: 3, color: "#fbd60a" },
-      { title: 'Four', value: 60, key: 4, color: "#8c4e09" },
-      { title: 'Five', value: 60, key: 5, color: "#d93905" },
-      { title: 'Six', value: 60, key: 6, color: "#b91274" }
+      { title: 'geography', value: 60, key: 1, color:"#efefef" },
+      { title: 'film_and_tv', value: 60, key: 2, color: "#efefef" },
+      { title: 'history', value: 60, key: 3, color: "#efefef" },
+      { title: 'arts_and_literature', value: 60, key: 4, color: "#efefef" },
+      { title: 'science', value: 60, key: 5, color: "#efefef" },
+      { title: 'sport_and_leisure', value: 60, key: 6, color: "#efefef" }
     ]
 
       //if correct
-      //get the current value of the data array
-      // Array.push - 
     // data[5].color = "#dfdfdf";
 
   
