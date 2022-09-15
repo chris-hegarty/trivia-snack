8f7f5d1886af9e78ec1beb084952117a23a175d6
chegarty74 <chegarty74@yahoo.com>
2022-09-15 16:16:54 -0500

    Menu updates and Register page functionalit fixes.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,52 +1,49 @@
-import React, { useContext } from 'react'
-import { NavLink } from 'react-router-dom'
-import {PlayerContext} from "../context/PlayerContext"
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { PlayerContext } from "../context/PlayerContext";
 
 function Menu() {
-
-    const{loggedInPlayer, logout} = useContext(PlayerContext);
-
-  return (
-    <>
-    <nav>
-        <ul className="flex flex-end p-3">
-            { !loggedInPlayer && (
-                <>
-                    <li className="p-2">
-                        <NavLink 
-                        className=" hover:bg-violet-600"
-                        to={"/register"}>Register</NavLink>
-                    </li>
-                    <li className="p-2">
-                        <NavLink 
-                        className=" hover:bg-violet-600"
-                        to={"/login"}>Login</NavLink>
-                    </li>
-                </>
-                )
-            }
-            { loggedInPlayer && (
-                <>
-                    <li>
-                        <NavLink to={"/game"}>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                                  className="hover:bg-violet-600"
-                            onClick={() => {
-                                logout()
-                            }}
-                            to={"/logout"}>
-                            Logout
-                        </NavLink>
-                    </li>
-                </>
-                )   
-            }
-        </ul>
-    </nav>
-    </>
-  )
+	const { loggedInPlayer, logout } = useContext(PlayerContext);
+	return (
+		<>
+			<nav>
+				<ul className="flex flex-end p-3">
+					{!loggedInPlayer && (
+						<>
+							<li className="p-2">
+								<NavLink className=" hover:bg-violet-600" to={"/register"}>
+									Register
+								</NavLink>
+							</li>
+							<li className="p-2">
+								<NavLink className=" hover:bg-violet-600" to={"/login"}>
+									Login
+								</NavLink>
+							</li>
+						</>
+					)}
+					{loggedInPlayer && (
+						<>
+							<li>
+								<NavLink to={"/game"}>Home</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="hover:bg-violet-600"
+									onClick={() => {
+										logout();
+									}}
+									to={"/logout"}
+								>
+									Logout
+								</NavLink>
+							</li>
+						</>
+					)}
+				</ul>
+			</nav>
+		</>
+	);
 }
 
-export default Menu
+export default Menu;
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,89 +1,102 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { PlayerContext } from "../context/PlayerContext";
 
 function RegisterPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirm, setConfirm] = useState("");
-  const [show, setShow] = useState(false);
-  const { login } = useContext(PlayerContext)
-  const navigate = useNavigate();
-  //Need to add player context.
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirm, setConfirm] = useState("");
+	const [show, setShow] = useState(false);
+	const { login } = useContext(PlayerContext);
+	const navigate = useNavigate();
 
-  return (
+	const passError = useMemo(
+		() => password.length < 8 || password.length > 30,
+		[password],
+	);
+	const userError = useMemo(
+		() => username.length < 4 || username.length > 20,
+		[username],
+	);
+	const confirmError = useMemo(
+		() => confirm !== password || passError,
+		[confirm, password, passError],
+	);
 
-    <main>
-      <section className="intro flex center align-center column">
-        <h1 className="gold">Trivia Time</h1>
-        <h2>A quiz game inspired by Trivial Pursuit.</h2>
-        <div className="register-page">
-          <div className="form-field">
-            <label htmlFor="user-name">Enter A Username</label>
-            <input 
-              value={username}
-              onChange={(e)=>{
-                setUsername(e.target.value)
-              }}
-              type="text"
-              id="user-name" 
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-          <div className="form-field">
-            <div className="flex space-between">
-            <label htmlFor="password">Enter a Password</label>
-            <span>
-              <label htmlFor="check-box">Show Password</label>
-              <input
-                value={show}
-                onChange={(e) => { setShow(e.target.checked) }}
-                type="checkbox"
-                name="register-checkbox"
-                id="register-check-box"
-                className="shadow appearance-none border  text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </span>
-            </div>
-              <input 
-                value={password}
-                onChange={(e)=>{
-                  setPassword(e.target.value)
-                }}
-                type={show === true ? "text" : "password"}
-                id="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-              
-          </div>
-          <div className="form-field">
-            <label htmlFor="confirm-password">Confirm Your Password</label>
-            <input
-              value={confirm}
-              onChange={(e) => {
-                setConfirm(e.target.value)
-              }}
-              type={show === true ? "text" : "password"}
-              id="confirm-password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
-          <button
-            className="rounded bg-[#daa520] py-1 px-6"
-            onClick={(e) => {
-              e.preventDefault()
-              console.log(password, confirm);
-              if (confirm === password) {
-                login(username)
-                navigate("/game")
-              }
-            }}>
-            Submit
-          </button>
-        </div>
-    </section>
-    </main>
-  )
+	return (
+		<main>
+			<section className="intro flex center align-center column">
+				<h1 className="gold">Trivia Time</h1>
+				<h2>A quiz game inspired by Trivial Pursuit.</h2>
+				<div className="register-page">
+					<div className="form-field">
+						<label htmlFor="user-name">Enter A Username</label>
+						<input
+							value={username}
+							onChange={(e) => {
+								setUsername(e.target.value);
+							}}
+							type="text"
+							id="user-name"
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						/>
+					</div>
+					<div className="form-field">
+						<div className="flex space-between">
+							<label htmlFor="password">Enter a Password</label>
+							<span>
+								<label htmlFor="check-box">Show Password</label>
+								<input
+									value={show}
+									onChange={(e) => {
+										setShow(e.target.checked);
+									}}
+									type="checkbox"
+									name="register-checkbox"
+									id="register-check-box"
+									className="shadow appearance-none border  text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								/>
+							</span>
+						</div>
+						<input
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
+							type={show === true ? "text" : "password"}
+							id="password"
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						/>
+					</div>
+					<div className="form-field">
+						<label htmlFor="confirm-password">Confirm Your Password</label>
+						<input
+							value={confirm}
+							onChange={(e) => {
+								setConfirm(e.target.value);
+							}}
+							type={show === true ? "text" : "password"}
+							id="confirm-password"
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						/>
+					</div>
+					<button
+						disabled={confirmError || passError || userError}
+						className="rounded bg-[#daa520] py-1 px-6"
+						onClick={(e) => {
+							e.preventDefault();
+							console.log(password, confirm);
+							if (!confirmError && !passError && !userError) {
+								navigate("/login");
+							}
+						}}
+					>
+						Submit
+					</button>
+				</div>
+			</section>
+		</main>
+	);
 }
 
-export default RegisterPage
+export default RegisterPage;
