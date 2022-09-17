25113568d3c88653ffc49eab10f310966b928b11
chegarty74 <chegarty74@yahoo.com>
2022-09-16 22:14:30 -0500

    correct sport color

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,36 +1,67 @@
 import React, { useContext, useMemo } from "react";
-import { PieChart } from 'react-minimal-pie-chart';
+import { PieChart } from "react-minimal-pie-chart";
 import { CardContext } from "../context/CardContext";
 
 function Wheel(props) {
-  
-  const {winList} = useContext(CardContext)
+	const { winList } = useContext(CardContext);
 
-  const data = useMemo(()=>
-    [
-      { title: 'geography', value: 60, key: 1, color: winList.geography ? "#05b4e1" : "#8c8c8c" },
-      { title: 'film_and_tv', value: 60, key: 2, color: winList.film_and_tv ? "#b91274" : "#8c8c8c" },
-      { title: 'history', value: 60, key: 3, color: winList.history ? "#fbd60a" : "#8c8c8c" },
-      { title: 'arts_and_literature', value: 60, key: 4, color: winList.arts_and_literature ? "#8c4e09" : "#8c8c8c" },
-      { title: 'science', value: 60, key: 5, color: winList.science ? "#6cac06" : "#8c8c8c" },
-      { title: 'sport_and_leisure', value: 60, key: 6, color: winList.sport_and_leisure ? "#b91274" : "#8c8c8c" }
-    ], [winList] )
-  
-  return (
-    <div className="basis-50 flex center">
-      <div id="wheel">
-      <PieChart
-        data={data}
-        totalValue={360}
-        paddingAngle={4}
-        background={"#daa520"}
-      />
-    </div>
-    </div>
-  )
+	const data = useMemo(
+		() => [
+			{
+				title: "geography",
+				value: 60,
+				key: 1,
+				color: winList.geography ? "#05b4e1" : "#8c8c8c",
+			},
+			{
+				title: "film_and_tv",
+				value: 60,
+				key: 2,
+				color: winList.film_and_tv ? "#b91274" : "#8c8c8c",
+			},
+			{
+				title: "history",
+				value: 60,
+				key: 3,
+				color: winList.history ? "#fbd60a" : "#8c8c8c",
+			},
+			{
+				title: "arts_and_literature",
+				value: 60,
+				key: 4,
+				color: winList.arts_and_literature ? "#8c4e09" : "#8c8c8c",
+			},
+			{
+				title: "science",
+				value: 60,
+				key: 5,
+				color: winList.science ? "#6cac06" : "#8c8c8c",
+			},
+			{
+				title: "sport_and_leisure",
+				value: 60,
+				key: 6,
+				color: winList.sport_and_leisure ? "#d93905" : "#8c8c8c",
+			},
+		],
+		[winList],
+	);
+
+	return (
+		<div className="basis-50 flex center">
+			<div id="wheel">
+				<PieChart
+					data={data}
+					totalValue={360}
+					paddingAngle={4}
+					background={"#daa520"}
+				/>
+			</div>
+		</div>
+	);
 }
 
-export default Wheel
+export default Wheel;
 
 //PATTERN for renaming an object key:
 
