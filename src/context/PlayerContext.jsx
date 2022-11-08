81ce6e6a231efb06442db8748b4b5a402419e646
chegarty74 <chegarty74@yahoo.com>
2022-11-07 22:18:43 -0600

    Set user to bypass login page.

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,25 +1,23 @@
-import React, { useState, createContext, useCallback } from 'react'
-export const PlayerContext = createContext(null)
+import React, { useState, createContext, useCallback } from "react";
+export const PlayerContext = createContext(null);
 
 export function PlayerProvider(props) {
+	const [loggedInPlayer, setLoggedInPlayer] = useState("Chris");
 
-    const [loggedInPlayer, setLoggedInPlayer] = useState("")
+	const login = useCallback(
+		(username) => {
+			setLoggedInPlayer(username);
+		},
+		[setLoggedInPlayer],
+	);
 
-    const login = useCallback(
-        (username) => {
-            setLoggedInPlayer(username)  
-        }, [setLoggedInPlayer]
-    );
+	const logout = useCallback(() => {
+		setLoggedInPlayer(null);
+	}, [setLoggedInPlayer]);
 
-    const logout = useCallback(
-        () => { 
-        setLoggedInPlayer(null);
-        }, [setLoggedInPlayer]
-    );
-
-  return <PlayerContext.Provider value={ {loggedInPlayer, login, logout} }>
-
-    {props.children}
-    
-    </PlayerContext.Provider>
+	return (
+		<PlayerContext.Provider value={{ loggedInPlayer, login, logout }}>
+			{props.children}
+		</PlayerContext.Provider>
+	);
 }
